b582bbb772dbfe400a4cebc8d7ecd19785698c8d
Roman Andoni <roman.andoni@mail.mcgill.ca>
2016-11-26 18:39:21 -0500

    Fuckennnn

---
+++ hid_test/USB_HID_Test/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'USB_HID_Test' 
+ * Target:  'USB_HID_Test Configuration' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
